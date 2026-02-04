60736690d4520a308d74099ec42773ca45b43bff
Steven R. Brandt <sbrandt@cct.lsu.edu>
2026-02-04 11:11:19 -0600

    Attempt to use batch_cse in place of regular cse to cut down on temporaries.

---
+++ batch_cse.py
@@ -0,0 +1,54 @@
+from sympy import *
+
+class SymIter:
+    def __init__(self, x):
+        self.x = x
+        self.n = 0
+    def __iter__(self):
+        self.n += 1
+        return self
+    def __next__(self):
+        return Symbol(f"{self.x}{self.n}")
+
+
+def batch_cse(e, batch=3, symbols=None):
+    n = 0
+    name_iter = SymIter(f"tmp_{n}_")
+    elist = []
+    out = []
+    out2 = []
+    for i in range(len(e)):
+        elist.append(e[i])
+        a, b = cse(elist, symbols=name_iter)
+        if len(a) > batch:
+            print("a:",a)
+            out += a
+            out2 += b
+            elist = []
+            n += 1
+            name_iter = SymIter(f"tmp_{n}_")
+    if len(elist) > 0:
+        a, b = cse(elist, symbols=name_iter)
+        out += a
+        out2 += b
+    return (out, out2)
+
+    return cse(e, symbols=s)
+
+if __name__ == "__main__":
+    u = Symbol("u")
+    v = Symbol("v")
+    e = [
+        (u**n + n*v**n)*(u**n + n*v**n + n**3)
+        for n in range(1,20)]
+    a, b = batch_cse(e)
+    for item in a:
+        print(">>",item)
+    for item in b:
+        print(item)
+    print()
+    a, b = cse(e)
+    for item in a:
+        print(">>",item)
+    for item in b:
+        print(item)
