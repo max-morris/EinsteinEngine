1be9b5d095e8b45cffe0a61d265f8bb95c272274
Max Morris <mmor115@lsu.edu>
2026-02-19 13:55:32 -0600

    Changed SyncMode enum to reflect new behavior.

--- recipes/Cottonmouth/bssnok_unsplit_ricci.py
+++ recipes/Cottonmouth/bssnok_unsplit_ricci.py
@@ -821,7 +821,7 @@ CppCarpetXWizard(
     cottonmouth_bssnok,
     CppCarpetXGenerator(
         cottonmouth_bssnok,
-        interior_sync_mode=InteriorSyncMode.MixedRhs,
+        sync_mode=SyncMode.EmulatePresync,
         interior_sync_schedule_target=post_step_group,
         extra_schedule_blocks=[
             initial_group,
