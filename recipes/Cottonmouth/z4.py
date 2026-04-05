01d3c6b4d4b94a47a0f680e0e3fd51e98f6514ef
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2026-04-04 19:44:58 -0500

    Cottonmouth: Prevent eta_B being an integer in Z4

--- recipes/Cottonmouth/z4.py
+++ recipes/Cottonmouth/z4.py
@@ -74,7 +74,7 @@ shift_B_f = cottonmouth_z4.add_param(
 
 eta_B = cottonmouth_z4.add_param(
     "eta_B",
-    default=2,
+    default=2.0,
     desc="Standard Gamma driver eta gauge parameter. Must be of order 2 / M_ADM"
 )
 
