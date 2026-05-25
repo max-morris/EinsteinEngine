3efe3f09d75d4835693e33b6f58a27b9eff4fa14
Max Morris <mmor115@lsu.edu>
2026-05-25 18:25:19 -0500

    Finishing up f90 generation.

--- EinsteinEngine/generators/vanilla_f90_generator.py
+++ EinsteinEngine/generators/vanilla_f90_generator.py
@@ -15,7 +15,10 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
+from typing import Literal, TypeAlias
+
+from EinsteinEngine.generators.generator_exception import GeneratorException
 
 from EinsteinEngine.emit.code.common.code_tree import BinOp, IdExpr
 from EinsteinEngine.generators.substitute_recycled_temporaries import substitute_recycled_temporaries
@@ -25,32 +28,34 @@ from EinsteinEngine.emit.ccl.schedule.schedule_tree import IntentRegion
 from EinsteinEngine.frontend.dsl.f90.vanilla_f90_frontend import VanillaF90Frontend
 from EinsteinEngine.common.util import OrderedSet
 from EinsteinEngine.emit.code.f90.f90_tree import F90CodeRoot, VarDecl, PrimitiveType, IntentIn, F90TopLevelNode
-from EinsteinEngine.emit.code.common.code_tree import BinOpExpr, IntLiteralExpr
+from EinsteinEngine.emit.code.common.code_tree import BinOpExpr, IntLiteralExpr, FloatLiteralExpr
 from EinsteinEngine.emit.code.f90.f90_sympy_visitor import F90SympyVisitor
 from EinsteinEngine.emit.code.f90.f90_tree import F90CodeElem, SubroutineDecl, TypeSpecifier, Dimension, DoLoop, Assignment, F90ExprNode
 from EinsteinEngine.generators.dsl_generator import DslGenerator
 
 import sympy as sy
 
+from EinsteinEngine.emit.code.f90.f90_tree import IntentOut
+
+ThornFnName: TypeAlias = str
+SymbolName: TypeAlias = str
 
 class VanillaF90Generator(DslGenerator[VanillaF90Frontend]):
     grid_names: OrderedSet[str]
-    read_decls: OrderedDict[str, IntentRegion]
-    write_decls: OrderedDict[str, IntentRegion]
-    local_temp_names: OrderedSet[str]
-    tile_temp_names: OrderedSet[str]
-
-    _sympy_visitor: F90SympyVisitor
+    read_decls: dict[ThornFnName, OrderedDict[SymbolName, IntentRegion]]
+    write_decls: dict[ThornFnName, OrderedDict[SymbolName, IntentRegion]]
+    local_temp_names: dict[ThornFnName, OrderedSet[SymbolName]]
+    tile_temp_names: dict[ThornFnName, OrderedSet[SymbolName]]
 
     vars_to_ignore: set[str] = {'t', 'x', 'y', 'z', 'DXI', 'DYI', 'DZI'}
 
     def __init__(self, frontend: VanillaF90Frontend):
         super().__init__(frontend)
         self.grid_names = OrderedSet()
-        self.read_decls = OrderedDict()
-        self.write_decls = OrderedDict()
-        self.local_temp_names = OrderedSet()
-        self.tile_temp_names = OrderedSet()
+        self.read_decls = defaultdict(OrderedDict)
+        self.write_decls = defaultdict(OrderedDict)
+        self.local_temp_names = defaultdict(OrderedSet)
+        self.tile_temp_names = defaultdict(OrderedSet)
 
         for tf in self.frontend.functions.values():
             tf.eqn_complex._calc_tile_temps()
@@ -60,30 +65,35 @@ class VanillaF90Generator(DslGenerator[VanillaF90Frontend]):
                 var_name = str(symbol).replace("'", "")
                 if var_name not in self.vars_to_ignore:
                     self.grid_names.add(var_name)
-                    self.read_decls[var_name] = region
+                    self.read_decls[tf.name][var_name] = region
 
             for symbol, region in tf.eqn_complex.write_decls.items():
                 var_name = str(symbol).replace("'", "")
                 if var_name not in self.vars_to_ignore:
                     self.grid_names.add(var_name)
-                    self.write_decls[var_name] = region
+                    self.write_decls[tf.name][var_name] = region
 
             for tt in tf.eqn_complex.tile_temporaries:
-                self.tile_temp_names.add(str(tt).replace("'", ""))
+                self.tile_temp_names[tf.name].add(str(tt).replace("'", ""))
 
             local_temps = tf.eqn_complex.temporaries - tf.eqn_complex.tile_temporaries
 
             for lt in local_temps:
-                self.local_temp_names.add(lt_name := str(lt).replace("'", ""))
-                assert lt_name not in self.tile_temp_names
+                self.local_temp_names[tf.name].add(lt_name := str(lt).replace("'", ""))
+                assert lt_name not in self.tile_temp_names[tf.name]
 
-            def should_inject_array_access(name: str, in_stencil_args: bool) -> bool:
-                return not in_stencil_args and (name in self.grid_names or name in self.tile_temp_names)
-
-            self._sympy_visitor = F90SympyVisitor(
-                stencil_fns={'stencil'},
-                should_inject_array_access=should_inject_array_access
+    def _mk_sympy_visitor(self, fn_name: ThornFnName) -> F90SympyVisitor:
+        def should_inject_array_access(name: str, in_stencil_args: bool) -> bool:
+            return not in_stencil_args and (
+                    name in self.grid_names
+                    or name in self.tile_temp_names[fn_name]
+                    or name in self.local_temp_names[fn_name]
             )
+
+        return F90SympyVisitor(
+            stencil_fns={'stencil'},
+            should_inject_array_access=should_inject_array_access
+        )
 
     def generate_function_code(self, which_fn: str) -> F90CodeRoot:
         nodes: list[F90CodeElem] = list()
@@ -91,6 +101,9 @@ class VanillaF90Generator(DslGenerator[VanillaF90Frontend]):
         fn_name: str = fn.name
 
         assert fn.been_baked
+
+        if len(self.frontend.global_temporaries) > 0:
+            raise GeneratorException("Global temporaries are not supported in vanilla f90")
 
         boilerplate_decls = [
             VarDecl(
@@ -106,9 +119,19 @@ class VanillaF90Generator(DslGenerator[VanillaF90Frontend]):
                         'ngx',
                         'ngy',
                         'ngz',
-                        'i',  # loop indices
-                        'j',  # "
-                        'k'   # "
+                    )
+                )
+            ),
+            VarDecl(
+                type=TypeSpecifier(
+                    type=PrimitiveType.Integer,
+                    attributes=[]
+                ),
+                names=list(
+                    Identifier(s) for s in (
+                        'i',
+                        'j',
+                        'k',
                     )
                 )
             ),
@@ -125,7 +148,32 @@ class VanillaF90Generator(DslGenerator[VanillaF90Frontend]):
                         'dt',
                     )
                 )
+            ),
+            VarDecl(
+                type=TypeSpecifier(
+                    type=PrimitiveType.Double,
+                    attributes=[]
+                ),
+                names=list(
+                    Identifier(s) for s in (
+                        'DXI',
+                        'DYI',
+                        'DZI'
+                    )
+                )
             )
+        ]
+
+        boilerplate_inits = [
+            Assignment(
+                Identifier(f'D{d.upper()}I'),
+                None,
+                BinOpExpr(
+                    FloatLiteralExpr(1.0),
+                    BinOp.Div,
+                    IdExpr(Identifier(f'd{d}'))
+                )
+            ) for d in ('x', 'y', 'z')
         ]
 
         #base_to_grid_vars: OrderedDict[str, set[str]] = OrderedDict()
@@ -139,14 +187,14 @@ class VanillaF90Generator(DslGenerator[VanillaF90Frontend]):
                     attributes=[IntentIn(), Dimension((None, None, None))]
                 ),
                 names=[Identifier(var)],
-            ) for var in self.read_decls.keys()),
+            ) for var in self.read_decls[fn_name].keys()),
             *(VarDecl(
                 type=TypeSpecifier(
                     type=PrimitiveType.Double,
-                    attributes=[IntentIn(), Dimension((None, None, None))]
+                    attributes=[IntentOut(), Dimension((None, None, None))]
                 ),
                 names=[Identifier(var)],
-            ) for var in self.write_decls.keys()),
+            ) for var in self.write_decls[fn_name].keys()),
         ]
 
         param_decls = [
@@ -167,6 +215,7 @@ class VanillaF90Generator(DslGenerator[VanillaF90Frontend]):
         ]
 
         loops: list[F90TopLevelNode] = list()
+        sympy_visitor = self._mk_sympy_visitor(fn.name)
         for loop_idx, eqn_list in enumerate(fn.eqn_complex.eqn_lists):
             output_region = loop_to_output_region[loop_idx]
             subst_result = substitute_recycled_temporaries(eqn_list)
@@ -175,18 +224,18 @@ class VanillaF90Generator(DslGenerator[VanillaF90Frontend]):
             def _resolve_overwrite(s: sy.Symbol) -> sy.Symbol:
                 return s if "'" not in str(s) else sy.Symbol(str(s).replace("'", ""))  # type: ignore[no-untyped-call]
 
-            eqns: list[tuple[sy.Symbol, F90ExprNode]] = [(_resolve_overwrite(lhs), self._sympy_visitor.visit(rhs)) for lhs, rhs in subst_result.eqns]
+            eqns: list[tuple[sy.Symbol, F90ExprNode]] = [(_resolve_overwrite(lhs), sympy_visitor.visit(rhs)) for lhs, rhs in subst_result.eqns]
             annotations: dict[str, str] = {str(lhs): ann for lhs, ann in fn.source_annotations.eqns[loop_idx].items()}
             temporaries = [
                 str(lhs) for lhs in OrderedSet(eqn_list.eqns.keys())
-                if lhs in (eqn_list.temporaries - self.frontend.global_temporaries - eqn_list.tile_temporaries) and str(lhs) not in self.grid_names
+                if lhs in (eqn_list.temporaries - self.frontend.global_temporaries) and str(lhs) not in self.grid_names
             ]
 
             for temp_name in temporaries:
                 temp_decls.append(VarDecl(
                     type=TypeSpecifier(
                         type=PrimitiveType.Double,
-                        attributes=[]
+                        attributes=[Dimension((None, None, None))]
                     ),
                     names=[Identifier(temp_name)],
                 ))
@@ -207,20 +256,40 @@ class VanillaF90Generator(DslGenerator[VanillaF90Frontend]):
                     )
                 )
 
-            # todo: this assumes output_region of Interior
+            def mk_loop_bounds(direction: Literal['x', 'y', 'z']) -> tuple[F90ExprNode, F90ExprNode]:
+                if output_region == IntentRegion.Interior:
+                    return (
+                        BinOpExpr(IdExpr(Identifier(f'ng{direction}')), BinOp.Add, IntLiteralExpr(1)),
+                        BinOpExpr(IdExpr(Identifier(f'n{direction}')), BinOp.Sub, IdExpr(Identifier(f'ng{direction}')))
+                    )
+                elif output_region == IntentRegion.Everywhere:
+                    return (
+                        IntLiteralExpr(1),
+                        IdExpr(Identifier(f'n{direction}'))
+                    )
+                else:
+                    raise GeneratorException(f"Unsupported output region: {output_region}")
+
+            k_bounds, j_bounds, i_bounds = mk_loop_bounds('z'), mk_loop_bounds('y'), mk_loop_bounds('x')
+
             loops.append(
                 DoLoop(
-                    Identifier('j'),
-                    BinOpExpr(IdExpr(Identifier('ngy')), BinOp.Add, IntLiteralExpr(1)),
-                    BinOpExpr(IdExpr(Identifier('ny')), BinOp.Sub, IdExpr(Identifier('ngy'))),
+                    Identifier('k'),
+                    *k_bounds,
                     step=None,
                     body=[
                         DoLoop(
-                            Identifier('i'),
-                            BinOpExpr(IdExpr(Identifier('ngx')), BinOp.Add, IntLiteralExpr(1)),
-                            BinOpExpr(IdExpr(Identifier('nx')), BinOp.Sub, IdExpr(Identifier('ngx'))),
+                            Identifier('j'),
+                            *j_bounds,
                             step=None,
-                            body=loop_body
+                            body=[
+                                DoLoop(
+                                    Identifier('i'),
+                                    *i_bounds,
+                                    step=None,
+                                    body=loop_body
+                                )
+                            ]
                         )
                     ]
                 )
@@ -240,7 +309,7 @@ class VanillaF90Generator(DslGenerator[VanillaF90Frontend]):
                         'dx',   # grid spacing in each direction
                         'dy',   # "
                         'dz',   # "
-                        'dt',    # time step size
+                        'dt',   # time step size
                         *self.grid_names,
                         *self.frontend.params.keys()
                     )
@@ -250,6 +319,7 @@ class VanillaF90Generator(DslGenerator[VanillaF90Frontend]):
                     *grid_decls,
                     *param_decls,
                     *temp_decls,
+                    *boilerplate_inits,
                     *loops
                 ]
             )
